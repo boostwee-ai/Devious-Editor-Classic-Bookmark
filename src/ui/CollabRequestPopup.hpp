3681e4a19feafd4641181c81d3249e28ec2afe05
Bishop <bishop@example.com>
2026-04-09 02:22:15 -0400

    Fix compilation: remove invalid override from custom init signature

--- src/ui/CollabRequestPopup.hpp
+++ src/ui/CollabRequestPopup.hpp
@@ -9,7 +9,7 @@ namespace ui {
 class CollabRequestPopup : public geode::Popup {
     std::string m_username;
 protected:
-    bool init(float width, float height, std::string const& username) override;
+    bool init(float width, float height, std::string const& username);
 
     void onYes(cocos2d::CCObject*);
     void onNo(cocos2d::CCObject*);
