8f649ee920d29c8b8fb3864e5acf01e0d64f52dd
Bishop <bishop@example.com>
2026-04-09 01:56:21 -0400

    Fix compilation: explicit FLAlertLayer::show() and project-wide winsock macros

--- src/ui/CollabButton.cpp
+++ src/ui/CollabButton.cpp
@@ -7,7 +7,7 @@ namespace ui {
 
 void CollabButton::onCollabButton(cocos2d::CCObject* sender) {
     auto p = ui::PlayerBrowserLayer::create();
-    if (p) p->show();
+    if (p) p->FLAlertLayer::show();
 }
 
 } // namespace ui
