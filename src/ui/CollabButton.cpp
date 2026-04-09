40279018477a92f8d6d6d0ce7a031c098100752f
Bishop <bishop@example.com>
2026-04-09 02:05:31 -0400

    Fix compilation: resolve visibility in templates and use project-wide macros

--- src/ui/CollabButton.cpp
+++ src/ui/CollabButton.cpp
@@ -8,9 +8,7 @@ namespace ui {
 
 void CollabButton::onCollabButton(cocos2d::CCObject* sender) {
     auto p = ui::PlayerBrowserLayer::create();
-    if (p) {
-        static_cast<FLAlertLayer*>(p)->show();
-    }
+    if (p) p->show();
 }
 
 } // namespace ui
--- src/ui/CollabRequestPopup.hpp
+++ src/ui/CollabRequestPopup.hpp
@@ -7,8 +7,10 @@
 namespace ui {
 
 class CollabRequestPopup : public geode::Popup<std::string const&> {
+public:
     using geode::Popup<std::string const&>::m_mainLayer;
     using geode::Popup<std::string const&>::m_noElasticity;
+    using geode::Popup<std::string const&>::show;
 protected:
     bool setup(std::string const& username) override;
 
