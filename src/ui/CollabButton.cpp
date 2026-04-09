1db9bcd5286678dddb07154f8bfb4820883f5a68
Bishop <bishop@example.com>
2026-04-09 02:00:11 -0400

    Fix compilation: explicit casting to FLAlertLayer* for show() and global winsock macros

--- src/ui/CollabButton.cpp
+++ src/ui/CollabButton.cpp
@@ -1,5 +1,6 @@
 #include "CollabButton.hpp"
 #include "PlayerBrowserLayer.hpp"
+#include <Geode/binding/FLAlertLayer.hpp>
 
 using namespace geode::prelude;
 
@@ -7,7 +8,9 @@ namespace ui {
 
 void CollabButton::onCollabButton(cocos2d::CCObject* sender) {
     auto p = ui::PlayerBrowserLayer::create();
-    if (p) p->FLAlertLayer::show();
+    if (p) {
+        static_cast<FLAlertLayer*>(p)->show();
+    }
 }
 
 } // namespace ui
